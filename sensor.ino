74c42b8b1712422cd59e7cde3af50513d1f4a3cc
Thibalt-C <62816126+Thibalt-C@users.noreply.github.com>
2024-06-05 18:54:08 +0200

    Update sensor.ino

--- sensor.ino
+++ sensor.ino
@@ -1,70 +1,23 @@
-/*************************************************** 
-  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
-
-  Designed specifically to work with the Adafruit RTD Sensor
-  ----> https://www.adafruit.com/products/3328
-
-  This sensor uses SPI to communicate, 4 pins are required to  
-  interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
- ****************************************************/
-
 #include <Adafruit_MAX31865.h>
-
-// Use software SPI: CS, DI, DO, CLK
 Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
-// use hardware SPI, just pass in the CS pin
-//Adafruit_MAX31865 thermo = Adafruit_MAX31865(10);
-
-// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
 #define RREF      430.0
-// The 'nominal' 0-degrees-C resistance of the sensor
-// 100.0 for PT100, 1000.0 for PT1000
 #define RNOMINAL  100.0
-
-// constants won't change. Used here to set a pin number:
-const int ledPin =  LED_BUILTIN;// the number of the LED pin
-int ledState = LOW;             // ledState used to set the LED
 
 
 void setup() {
   Serial.begin(19200);
-  thermo.begin(MAX31865_2WIRE);  // set to 2WIRE or 4WIRE as necessary
-  //float previousValue = 0;
-  // set the digital pin as output:
-  pinMode(ledPin, OUTPUT);
+  thermo.begin(MAX31865_2WIRE);
 }
 
 
 void loop() {
   
-  // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
-    
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
   float pH = sensorValue;
-  
-  // print out the value you read:
-  //Serial.print('!');
   Serial.print(thermo.temperature(RNOMINAL, RREF));
   Serial.print(';');
   Serial.println(pH);
-
-  //if (abs(sensorValue-previousValue)>5): {
-  //  ledState = HIGH;
-  //}
-  //else {
-  //  ledState = LOW;
-  //}
-
-  //float previousValue = pH
   
-  // Check and print any faults
   uint8_t fault = thermo.readFault();
   if (fault) {
     Serial.print("Fault 0x"); Serial.println(fault, HEX);
@@ -88,5 +41,7 @@ void loop() {
     }
     thermo.clearFault();
   }
-  delay(5);
+  
+  delay(1);
+  
 }
